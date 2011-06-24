fc8b24a051854178d42e5e12fa60a160808456af
Greg Doermann <gdoermann@gmail.com>
2011-06-23 19:03:27 -0600

    Fixed a type in README

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@ __author__ = 'gdoermann'
 
 from distutils.core import setup
 
-setup(name='Django Limbo',
-      version='1.7.0',
-      description='A bunch of django libraries that hang in limbo...',
-      author='Gregory Doermann',
-      author_email='dev@doermann.me',
-      url='https://github.com/gdoermann/django-limbo',
-      packages=['limbo',],
-      requires=['django', ]
+setup(
+    name='Django Limbo',
+    version='1.7.0',
+    url='https://github.com/gdoermann/django-limbo',
+    author='Gregory Doermann',
+    author_email='dev@doermann.me',
+    summary='A bunch of django libraries that hang in limbo...',
+    description="""
+    This is a long list of libraries I have used in almost all of my projects.
+    This also includes altered code from django snippets and code I have copied directly from others.
+    """,
+    download_url='https://github.com/gdoermann/django-limbo/tarball/master',
+    license='https://github.com/gdoermann/django-limbo/blob/master/MIT-LICENSE.txt',
+    platform=['Any'],
+    packages=['limbo',],
+    requires=['django', ]
      )
